fe85aa4d774acdd49d640fe33e09ab83d9c0cef6
OlivierDupre <dupreolivier@gmail.com>
2013-04-02 22:41:25 +0200

    Initial commit - Résultat exact de notre COding Dojo - Départ anticipé

---
+++ public_html/js/romanTranslator.js
@@ -0,0 +1,69 @@
+var mapRomanNumbers = {
+    1000: "M",
+    100: "C",
+    10: "X",
+    9: "IX",
+    5: "V", 
+    4: "IV",
+    1: "I"
+};
+
+getGreatestRomanKey = function(decNb) {
+
+    var res;
+    for (var key in mapRomanNumbers) {
+        if (key <= decNb) {
+            res = key;
+        }
+    }
+    return res;
+};
+
+
+convert = function(decimalNumber) {
+
+    var romanNumber = "";
+
+    while (decimalNumber > 0) {
+        romanNumber += convertAllExcept14(decimalNumber);
+        decimalNumber -= getGreatestRomanKey(decimalNumber);
+    }
+
+//    if (decimalNumber === 14) {
+//        romanNumber = "X" + convertAllExcept14(decimalNumber - 10);
+//    } else {
+//        romanNumber = convertAllExcept14(decimalNumber);
+//    }
+
+
+
+    return romanNumber;
+};
+
+concatI = function(number) {
+    var romanNumber = "";
+    for (var i = 0; i < number; i++)
+    {
+        romanNumber += "I";
+    }
+    return romanNumber;
+};
+
+convertAllExcept14 = function(decimalNumber) {
+
+    var greatestRomanKey = getGreatestRomanKey(decimalNumber);
+    var romanChar = mapRomanNumbers[ greatestRomanKey ];
+
+    var romanNumber = "";
+    if (decimalNumber < greatestRomanKey) {
+        romanNumber = "I";
+    }
+
+    romanNumber += romanChar;
+
+    if (decimalNumber > greatestRomanKey) {
+        romanNumber += concatI(decimalNumber - greatestRomanKey);
+    }
+
+    return romanNumber;
+};
---
+++ public_html/js/test.js
@@ -0,0 +1,53 @@
+module("Chiffres décimaux");
+test( "1 retourne I", function() {
+  equal( convert(1), "I","Passed!" );
+});
+
+test( "2 retourne II", function() {
+  equal( convert(2), "II", "Passed!" );
+});
+
+test("3 retourne III", function() {
+    equal(convert(3), "III", "Passed!");
+});
+
+test("4 retourne IV", function() {
+    equal(convert(4), "IV", "Passed!");
+});
+
+test( "5 retourne V", function(){
+  equal( convert(5), "V", "Passed!" );
+});
+
+test( "6 retourne VI", function(){
+  equal( convert(6), "VI", "Passed!" );
+});
+
+test("8 return VIII", function() {
+   equal(convert(8), "VIII", "Passed!");
+});
+test("9 retourne IX", function() {
+    equal(convert(9), "IX", "Passed!");
+});
+
+module("Nombres décimaux");
+test("10 retourne X", function() {
+    equal(convert(10), "X", "Passed!");
+});
+test("12 retourne XII", function() {
+    equal(convert(12), "XII", "Passed!");
+});
+test("14 retourne XIV", function() {
+    equal(convert(14), "XIV", "Passed!");
+});
+test("19 retourne XIX", function() {
+    equal(convert(19), "XIX", "Passed!");
+});
+test("100 retourne C", function() {
+    equal(convert(100), "C", "Passed!");
+});
+test("1000 retourne M", function() {
+    equal(convert(1000), "M", "Passed!");
+});
+
+
